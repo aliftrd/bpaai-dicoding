ab4c93b84bf0a2245a3beb86b59396858a9b748d
Alif Triadi <aliftriadi.gaming@gmail.com>
2024-04-16 18:07:49 +0700

    Removed .idea files

--- app/src/main/java/com/github/aliftrd/sutori/ui/story/detail/DetailStoryViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.github.aliftrd.sutori.ui.story.detail
-
-import androidx.lifecycle.ViewModel
-import com.github.aliftrd.sutori.data.story.StoryRepository
-
-class DetailStoryViewModel(private val repository: StoryRepository): ViewModel() {
-
-}
